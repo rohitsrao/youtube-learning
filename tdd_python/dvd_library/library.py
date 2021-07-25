e81143fd4b5c4c1dc568a3db8f10b2613437e148
Rohit <rohitsrao@gmail.com>
2021-07-25 21:15:23 +0200

    Converted Library.catalouge to Library._catalouge

--- tdd_python/dvd_library/library.py
+++ tdd_python/dvd_library/library.py
@@ -3,12 +3,12 @@ from movie import Movie
 class Library:
 
     def __init__(self):
-        self.catalouge = []
+        self._catalouge = []
     
     def donate(self, movie):
-        self.catalouge.append(movie)
+        self._catalouge.append(movie)
         movie.add_copy()
 
     def contains(self, movie):
-        return (movie in self.catalouge)
+        return (movie in self._catalouge)
 
