a69243d525a0703e9ce55ab289b54bf53e8a0c3f
Rohit <rohitsrao@gmail.com>
2021-07-25 21:13:14 +0200

    Converted Movie.copies to a private property Movie._copies

--- tdd_python/dvd_library/movie.py
+++ tdd_python/dvd_library/movie.py
@@ -1,10 +1,10 @@
 class Movie:
 
     def __init__(self):
-        self.copies = 0
+        self._copies = 0
 
     def add_copy(self):
-        self.copies += 1
+        self._copies += 1
 
     def get_copies(self):
-        return self.copies
+        return self._copies
