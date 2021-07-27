155106daba7f213439b421d9203ad3d49b0268af
Rohit <rohitsrao@gmail.com>
2021-07-27 21:39:13 +0200

    Eliminated Stack.size and refactored

--- tdd_python/uncle_bob_tdd/01_unitycoin/stack.py
+++ tdd_python/uncle_bob_tdd/01_unitycoin/stack.py
@@ -4,24 +4,21 @@ class UnderflowError(Exception):
 class Stack():
 
     def __init__(self):
-        self.size = 0
         self.elements = []
 
     def is_empty(self):
-        if self.size == 0:
+        if len(self.elements) == 0:
             return True
         else:
             return False
 
     def push(self, element):
-        self.size += 1
         self.elements.append(element)
 
     def pop(self):
         if self.is_empty() == True:
             raise UnderflowError('Stack is Empty')
         else: 
-            self.size -= 1
             popped_element = self.elements[-1]
             del self.elements[-1]
             return popped_element
