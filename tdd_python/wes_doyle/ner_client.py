52bdbfe12b514e5a8b1da6b3068ee8a5681fb933
Rohit <rohitsrao@gmail.com>
2021-07-28 22:35:14 +0200

    test_get_ents_returns_dictionary_given_empty_input() passed

--- tdd_python/wes_doyle/ner_client.py
+++ tdd_python/wes_doyle/ner_client.py
@@ -2,4 +2,7 @@ class NamedEntityClient:
 
     def __init__(self):
         pass
+
+    def get_ents(self, sentence):
+        return {}
     
--- tdd_python/wes_doyle/test/test_ner_client.py
+++ tdd_python/wes_doyle/test/test_ner_client.py
@@ -1,5 +1,7 @@
 import unittest
 
+from ner_client import NamedEntityClient
+
 class TestNerClient(unittest.TestCase):
 
     def test_get_ents_returns_dictionary_given_empty_input(self):
