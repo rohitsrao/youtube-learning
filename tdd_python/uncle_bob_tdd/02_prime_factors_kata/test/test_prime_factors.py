806792e4522c6279cec553bb9a4048fb1a3c0bec
Rohit <rohitsrao@gmail.com>
2021-07-28 07:42:03 +0200

    asserts that facotrs(3) is [3]
    does this by the following logic
    if num is greater than 1 return a list with num

--- tdd_python/uncle_bob_tdd/02_prime_factors_kata/test/test_prime_factors.py
+++ tdd_python/uncle_bob_tdd/02_prime_factors_kata/test/test_prime_factors.py
@@ -2,8 +2,8 @@ import unittest
 
 def factorsOf(num):
     factors = []
-    if num == 2:
-        factors.append(2)
+    if num > 1:
+        factors.append(num)
     return factors
 
 class PrimeFactorsTest(unittest.TestCase):
@@ -11,6 +11,7 @@ class PrimeFactorsTest(unittest.TestCase):
     def test_factors(self):
         self.assertEqual(len(factorsOf(1)), 0)
         self.assertListEqual(factorsOf(2), [2])
+        self.assertListEqual(factorsOf(3), [3])
 
 if __name__ == '__main__':
     unittest.main()
