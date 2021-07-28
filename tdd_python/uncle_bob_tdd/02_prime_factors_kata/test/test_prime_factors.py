19a30882154035951cb0e2577ab6bf4af85d1e5d
Rohit <rohitsrao@gmail.com>
2021-07-28 07:39:59 +0200

    asserts that factorsOf(2) returns a list containing 2
    This is achieved by defining an empty factors list in factorsOf()
    if num is equal to 2 then it returns [2]
    this is wrong code but this is bare minimum that passes the test

--- tdd_python/uncle_bob_tdd/02_prime_factors_kata/test/test_prime_factors.py
+++ tdd_python/uncle_bob_tdd/02_prime_factors_kata/test/test_prime_factors.py
@@ -1,12 +1,16 @@
 import unittest
 
 def factorsOf(num):
-    return []
+    factors = []
+    if num == 2:
+        factors.append(2)
+    return factors
 
 class PrimeFactorsTest(unittest.TestCase):
 
     def test_factors(self):
         self.assertEqual(len(factorsOf(1)), 0)
+        self.assertListEqual(factorsOf(2), [2])
 
 if __name__ == '__main__':
     unittest.main()
