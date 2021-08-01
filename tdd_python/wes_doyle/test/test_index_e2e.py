7968ff6ccfa2ee575c9201d930e1d8f03ed93a3d
Rohit <rohitsrao@gmail.com>
2021-08-01 08:10:42 +0200

    Browser page title tested and passed
    Created app.py which renders index.html on root request
    Created index.html which sets the title of the page
    Updated setUp in E2ETests to now include brave browser and chrome driver

--- tdd_python/wes_doyle/test/test_index_e2e.py
+++ tdd_python/wes_doyle/test/test_index_e2e.py
@@ -5,8 +5,13 @@ from selenium import webdriver
 class E2ETests(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path = r'/home/rohit/Downloads/chromedriver')
+        self.options = webdriver.ChromeOptions()
+        self.options.binary_location = '/usr/bin/brave-browser'
+        self.driver = webdriver.Chrome(options = self.options, executable_path = r'/home/rohit/Downloads/chromedriver')
         self.driver.get('http://localhost:5000')
 
     def tearDown(self):
         self.driver.quit()
+
+    def test_browser_title_contains_app_name(self):
+        self.assertIn('Named Entity', self.driver.title)
