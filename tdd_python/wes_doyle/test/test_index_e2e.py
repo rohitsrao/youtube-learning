0438c8e311b2b85602b9913a6a73a2a0b3908456
Rohit <rohitsrao@gmail.com>
2021-08-01 08:32:29 +0200

    Test for heading defind and passed
    Defind a test specific id in html such that front end developers
    can assign different ids for css javascript without affecting the test

--- tdd_python/wes_doyle/test/test_index_e2e.py
+++ tdd_python/wes_doyle/test/test_index_e2e.py
@@ -10,8 +10,17 @@ class E2ETests(unittest.TestCase):
         self.driver = webdriver.Chrome(options = self.options, executable_path = r'/home/rohit/Downloads/chromedriver')
         self.driver.get('http://localhost:5000')
 
-    def tearDown(self):
-        self.driver.quit()
-
     def test_browser_title_contains_app_name(self):
         self.assertIn('Named Entity', self.driver.title)
+
+    def test_page_heading_is_named_entity_finder(self):
+        heading = self._find("heading").text
+        self.assertEqual('Named Entity Finder', heading)
+
+    def _find(self, val):
+        return self.driver.find_element_by_css_selector(f'[data-test-id="{val}"]')
+
+    def tearDown(self):
+        print('TearDown entered')
+        self.driver.quit()
+
