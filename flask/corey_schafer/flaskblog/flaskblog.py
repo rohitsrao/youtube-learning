e4555b1d23fee0a97e608a2e085e18c4aedbff3e
Rohit <rohitsrao@gmail.com>
2021-09-07 21:41:02 +0200

    Can run flask server from within Python and added about route

--- flask/corey_schafer/flaskblog/flaskblog.py
+++ flask/corey_schafer/flaskblog/flaskblog.py
@@ -3,4 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return '<h1>Home Page</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
