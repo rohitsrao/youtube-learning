29f2f4e7793fa2e1757bba2238edda8e34017486
Rohit <rohitsrao@gmail.com>
2021-09-07 22:31:05 +0200

    Introduced templates for about and home.
    Home page now displays some dummy blog posts

--- flask/corey_schafer/flaskblog/flaskblog.py
+++ flask/corey_schafer/flaskblog/flaskblog.py
@@ -1,14 +1,37 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Rohit Rao',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Sep 7, 2021',
+    },
+    {
+        'author': 'Kristina Yanushko',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Sep 8, 2021',
+    },
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template(
+        'home.html',
+        posts = posts,
+    )
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template(
+        'about.html',
+        title='About'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
