0c08bb7514679af20ca645d757ab358de93f1db9
Rohit <rohitsrao@gmail.com>
2021-09-07 17:50:32 +0200

    Hello World html now displays name

--- django/mosh/storefront/playground/views.py
+++ django/mosh/storefront/playground/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse('Hello World')
+    return render(
+        request, 
+        'hello.html', 
+        {
+            'name': 'Rohit',
+        }
+    )
