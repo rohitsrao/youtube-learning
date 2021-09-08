e5338e211ef22bd323c8f3bb9b16c8aaffc8028a
Rohit <rohitsrao@gmail.com>
2021-09-08 19:13:38 +0200

    Converted to a package

---
+++ flask/corey_schafer/flaskblog/flaskblog/__init__.py
@@ -0,0 +1,12 @@
+from flaskblog import config
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+from flaskblog import routes
--- flask/corey_schafer/flaskblog/flaskblog/routes.py
+++ flask/corey_schafer/flaskblog/flaskblog/routes.py
@@ -1,38 +1,7 @@
-import config
-
-
-from datetime import datetime
-from flask import Flask
 from flask import flash, url_for, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = config.SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-    
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), unique=True, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
 
 posts = [
     {
@@ -48,6 +17,7 @@ posts = [
         'date_posted': 'Sep 8, 2021',
     },
 ]
+
 
 @app.route('/')
 @app.route('/home')
@@ -94,6 +64,3 @@ def login():
         title = 'Login',
         form = form,
     )
-
-if __name__ == '__main__':
-    app.run(debug=True)
