1fa518d8cc76ea2378a717a46d2a5206bb28e68d
Rohit <rohitsrao@gmail.com>
2021-09-08 22:47:49 +0200

    Account image can now be changed

--- flask/corey_schafer/flaskblog/flaskblog/forms.py
+++ flask/corey_schafer/flaskblog/flaskblog/forms.py
@@ -1,5 +1,7 @@
 from flaskblog.models import User
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 
@@ -63,3 +65,42 @@ class LoginForm(FlaskForm):
     )
     submit = SubmitField('Login')
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=20),
+        ]
+    )
+
+    email = StringField(
+        'Email',
+        validators = [
+            DataRequired(),
+            Email(),
+        ]
+    )
+
+    picture = FileField(
+        'Update Profile Picture',
+        validators = [
+            FileAllowed(['jpg', 'jpeg', 'png'])
+        ]
+    )
+
+    submit = SubmitField(
+        'Update'
+    )
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError('That username is already taken. Please choose another one')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('That email address is already taken. Please choose another one')
