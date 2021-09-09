2fd63678a731fa32ee9c27f2a07480c2ca873d77
Rohit <rohitsrao@gmail.com>
2021-09-09 18:34:23 +0200

    Creating app using a separate function

--- flask/corey_schafer/flaskblog/flaskblog/__init__.py
+++ flask/corey_schafer/flaskblog/flaskblog/__init__.py
@@ -6,22 +6,31 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 bcrypt = Bcrypt()
 
-login_manager = LoginManager(app)
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-mail = Mail(app)
+mail = Mail()
 
-from flaskblog.users.routes import users
-from flaskblog.posts.routes import posts
-from flaskblog.main.routes import main
+def create_app(config_class = Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    
+    return app
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
