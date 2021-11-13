cf63160116fc81dfb766d062222f5992413c13eb
Rohit Rao <rohit@rohitsrao.com>
2021-11-13 05:37:49 +0100

    Added a basic Home component

---
+++ react/fcc/rmdb/src/components/Home/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useState } from 'react';
+
+//Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../../config';
+
+//Components
+
+//Hooks
+
+
+//Images
+import NoImage from '../../images/no_image.jpg';
+
+
+const Home = () => {
+
+  const [state, setState] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  return <div>Home Page</div>
+};
+
+export default Home;
