60b2f08e231051565be9b5431376da9861cd7edb
Rohit Rao <rohit@rohitsrao.com>
2021-11-13 15:00:06 +0100

    Created a custom hook and extracted code into useHomeFetch.js

--- react/fcc/rmdb/src/components/Home/index.js
+++ react/fcc/rmdb/src/components/Home/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-//API
-import API from '../../API';
-
 //Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../../config';
 
 //Components
 
 //Hooks
+import { useHomeFetch } from '../../hooks/useHomeFetch';
 
 //Images
 import NoImage from '../../images/no_image.jpg';
@@ -17,42 +15,8 @@ import NoImage from '../../images/no_image.jpg';
 
 const Home = () => {
 
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { state, loading, error } = useHomeFetch();
 
-  const fetchMovies = async (page, searchTerm) => {
-    
-    try {
-      setError(false);
-      setLoading(true);
-      
-      const movies = await API.fetchMovies(searchTerm, page);
-      console.log('Movies from API');
-      console.log(movies);
-
-      setState((prev) => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-      }));
-    }
-    catch (error) {
-      setError(true);
-    }
-    setLoading(false);
-
-  };
-
-  //Initial Render
-  //Fetch movies from first page
-  //Emtpy dependency array [] means it runs once on mount
-  //ie when the component loads for the first time
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
-
-  console.log('Printing State');
   console.log(state);
 
   return <div>Home Page</div>
