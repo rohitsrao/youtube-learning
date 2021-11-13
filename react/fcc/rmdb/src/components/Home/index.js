701434ac657054c489d07d6d1803e69e2a347958
Rohit Rao <rohit@rohitsrao.com>
2021-11-13 15:20:16 +0100

    Created a HeroImage Component without styling

--- react/fcc/rmdb/src/components/Home/index.js
+++ react/fcc/rmdb/src/components/Home/index.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../../config';
 
 //Components
+import HeroImage from '../HeroImage';
 
 //Hooks
 import { useHomeFetch } from '../../hooks/useHomeFetch';
@@ -19,7 +20,19 @@ const Home = () => {
 
   console.log(state);
 
-  return <div>Home Page</div>
+  return (
+    <>
+      {state.results[0]
+        ? <HeroImage 
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            title={state.results[0].original_title}
+            text={state.results[0].overview}
+          />
+        : null
+      }
+    </>
+  );
+
 };
 
 export default Home;
