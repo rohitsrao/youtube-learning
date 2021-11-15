a9f252447a00a7f6b5ee06e4ea95c669df2ac368
Rohit Rao <rohit@rohitsrao.com>
2021-11-15 17:35:59 +0100

    Added BreadCrumb without styling

--- react/fcc/rmdb/src/components/Movie/index.js
+++ react/fcc/rmdb/src/components/Movie/index.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
 
 //Components
+import BreadCrumb from '../BreadCrumb';
 import Grid from '../Grid';
 import Spinner from '../Spinner';
 
@@ -18,11 +19,13 @@ const Movie = () => {
 
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
-  console.log(movie);
+
+  if (error) return <div>Something went wrong ...</div>;
+  if (loading) return <Spinner />;
 
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
   );
 
