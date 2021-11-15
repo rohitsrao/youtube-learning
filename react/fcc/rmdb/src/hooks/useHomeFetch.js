cdd83c1767970c70535634f29d4c1fae9a2f4c04
Rohit Rao <rohit@rohitsrao.com>
2021-11-15 15:21:27 +0100

    Search hooked up to APi and UI optimized

--- react/fcc/rmdb/src/hooks/useHomeFetch.js
+++ react/fcc/rmdb/src/hooks/useHomeFetch.js
@@ -38,9 +38,10 @@ export const useHomeFetch = () => {
   };
 
   useEffect(() => {
-    fetchMovies(1);
-  }, []);
+    setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
 
-  return { state, loading, error, setSearchTerm };
+  return { state, loading, error, searchTerm, setSearchTerm };
 
 };
