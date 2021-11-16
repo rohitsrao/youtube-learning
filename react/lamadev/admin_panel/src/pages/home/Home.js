0ef58742aedeeb81435982686cf6b41ff26a625e
Rohit Rao <rohit@rohitsrao.com>
2021-11-16 10:59:19 +0100

    Added a small and large widget at bottom of home page

--- react/lamadev/admin_panel/src/pages/home/Home.js
+++ react/lamadev/admin_panel/src/pages/home/Home.js
@@ -1,11 +1,21 @@
+import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
 
 import './home.css';
+
+import { userData } from '../../dummyData';
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
     </div>
   );
 }
