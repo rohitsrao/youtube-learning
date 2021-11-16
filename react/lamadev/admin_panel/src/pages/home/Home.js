a21c25e0654d8694938bc91a9f760aef8c21fe3a
Rohit Rao <rohit@rohitsrao.com>
2021-11-16 10:23:12 +0100

    Added featuredInfo with Styling

--- react/lamadev/admin_panel/src/pages/home/Home.js
+++ react/lamadev/admin_panel/src/pages/home/Home.js
@@ -1,9 +1,11 @@
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+
 import './home.css';
 
 const Home = () => {
   return (
     <div className="home">
-      home
+      <FeaturedInfo />
     </div>
   );
 }
