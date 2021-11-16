7e0600ec15ba1d6b807a5ae1ae0690372005ed2c
Rohit Rao <rohit@rohitsrao.com>
2021-11-16 09:44:29 +0100

    Sidebar is complete

--- react/lamadev/admin_panel/src/App.js
+++ react/lamadev/admin_panel/src/App.js
@@ -1,9 +1,18 @@
 import TopBar from './components/topbar/Topbar';
+import Sidebar from './components/sidebar/Sidebar';
+
+import './app.css';
 
 function App() {
   return (
     <div>
       <TopBar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">
+          other pages
+        </div>
+      </div>
     </div>
   );
 }
