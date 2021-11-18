9bcf72410de604907b909fad84c36347d5e47235
Rohit Rao <rohit@rohitsrao.com>
2021-11-18 10:12:23 +0100

    Added a second snapshot

--- react/techsith/buttontest/src/components/button/__test__/button.test.js
+++ react/techsith/buttontest/src/components/button/__test__/button.test.js
@@ -23,7 +23,12 @@ it('renders button correctly', () => {
   expect(getByTestId('button')).toHaveTextContent("save");
 });
 
-it('matches snapshot', () => {
+it('matches snapshot 1', () => {
   const tree = renderer.create(<Button label="save" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('matches snapshot 2', () => {
+  const tree = renderer.create(<Button label="click me" />).toJSON();
   expect(tree).toMatchSnapshot();
 });
